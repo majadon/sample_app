c064ae4a8f53f0754c4e58953893087438ea4f03
majadon <majadon@majadon.com>
2013-01-17 16:24:05 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
-
-
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'static_pages#home'
@@ -11,6 +8,8 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
+
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
